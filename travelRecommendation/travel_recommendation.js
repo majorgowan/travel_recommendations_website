0a69780e993b7f4570ebabb7b5a81f9a8d3d4d8c
Mark Fruman <majorgowan@yahoo.com>
2026-03-31 14:37:12 -0400

    implementing search recommendations

---
+++ travelRecommendation/travel_recommendation.js
@@ -0,0 +1,65 @@
+function fetchData(key) {
+    fetch("./travel_recommendation_api.json")
+        .then(response => response.json())
+        .then(data => {
+            displayRecommendations(data[key]);
+        })
+        .catch(err => console.log(err));
+}
+
+function displayRecommendations(data) {
+    console.log(data);
+    // hide blurb
+    const blurb = document.getElementById("blurb");
+    const searchResults = document.getElementById("searchResults");
+    const resultsContent = document.getElementById("resultsContent");
+
+    blurb.style.display = "none";
+
+    // clear existing search results
+    resultsContent.innerHTML = "";
+
+    // populate search results
+    for (const datum of data) {
+        resultsContent.appendChild(makeRecommendationCard(datum));
+    }
+
+    // show search results
+    searchResults.style.display = "block";
+}
+
+function makeRecommendationCard(datum) {
+    const card = document.createElement("div");
+    card.classList.add("recommendationCard");
+    let innerstring = `<h3>${datum.name}</h3>`;
+
+    // "countries" has different structure to other keys
+    if ("cities" in datum) {
+        for (const city of datum.cities) {
+            innerstring += `<h5>${city.name}</h5>`;
+            innerstring += `<img src='${city.imageUrl}'>`;
+            innerstring += `<p>${city.description}</p>`;
+        }
+    } else {
+        innerstring += `<img src='${datum.imageUrl}'>`;
+        innerstring += `<p>${datum.description}</p>`;
+    }
+
+    card.innerHTML = innerstring;
+    console.log(card);
+    console.log(innerstring);
+    return card;
+}
+
+function searchButtonClicked() {
+    const searchInput = document.getElementById("searchInput");
+    const searchTerm = searchInput.value;
+    if (searchTerm.toLowerCase().includes("beach")) {
+        const data = fetchData("beaches");
+    } else if (searchTerm.toLowerCase().includes("country")
+               || searchTerm.toLowerCase().includes("countries")) {
+        const data = fetchData("countries");
+    } else if (searchTerm.toLowerCase().includes("temple")) {
+        const data = fetchData("temples");
+    }
+}
